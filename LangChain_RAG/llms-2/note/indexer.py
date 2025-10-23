4ff1cf387a2eaaf68f97558644c605244fbcc635
Alkaid <1241002420@qq.com>
2025-10-23 20:46:28 +0800

    代码更新
    构建向量库：添加PDF、FAISS索引、更新indexer.py与依赖清单

--- LangChain_RAG/llms-2/note/indexer.py
+++ LangChain_RAG/llms-2/note/indexer.py
@@ -1,17 +1,24 @@
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import ModelScopeEmbeddings
 from langchain_community.vectorstores import FAISS
 
-# 解析PDF，切成chunk片段
-pdf_loader=PyPDFLoader('LLM.pdf',extract_images=True)   # 使用OCR解析pdf中图片里面的文字
-chunks=pdf_loader.load_and_split(text_splitter=RecursiveCharacterTextSplitter(chunk_size=100,chunk_overlap=10))
+# 1. 解析PDF，切成chunk片段
+pdf_loader = PyPDFLoader('Google Prompt Guidance.pdf', extract_images=True)   # 调用OCR解析pdf中图片里面的文字
 
-# 加载embedding模型，用于将chunk向量化
-embeddings=ModelScopeEmbeddings(model_id='iic/nlp_corom_sentence-embedding_chinese-base') 
+# 切片大小和重叠大小
+CHUNK_SIZE = 100
+CHUNK_OVERLAP = 10
 
-# 将chunk插入到faiss本地向量数据库
-vector_db=FAISS.from_documents(chunks,embeddings)
-vector_db.save_local('/kaggle/working/LLM.faiss')
+chunks = pdf_loader.load_and_split(
+	text_splitter=RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
+)   # 切成chunk片段，每 CHUNK_SIZE 个字符切一段，相邻段之间有 CHUNK_OVERLAP 个字符重叠
 
-print('faiss saved!')
+# 2. 加载embedding模型（直接调用该模型），用于将chunk向量化
+embeddings = ModelScopeEmbeddings(model_id='iic/nlp_corom_sentence-embedding_chinese-base')   #加载embedding模型，此处用的是中文句向量模型
+
+# 3. 将chunk插入到faiss本地向量数据库，faiss为向量索引工具，提供高效的向量索引和搜索功能
+vector_db = FAISS.from_documents(chunks,embeddings)
+vector_db.save_local('faiss_index/LLM')   #保存faiss本地向量数据库到指定的相对路径，即本代码所在的文件夹中，如不存在则会自动创建
+
+print('faiss saved!')   #帮助确认代码运行到此处
